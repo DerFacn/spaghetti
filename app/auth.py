7a39d06d46169a08fd6419859965d5cc113af9d9
DerFacn <derfacn.pro228@gmail.com>
2023-10-06 20:34:58 +0200

    New auth method
    Login created. Fixed issue with collect data from
    required.form

--- app/auth.py
+++ app/auth.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request
 from app.models import User
 from app import session
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
@@ -10,8 +10,8 @@ bp = Blueprint('auth', __name__, url_prefix='/auth')
 @bp.route('/register', methods=['POST'])
 def register():
     # Collect username and password
-    username = request.form['username']
-    password = request.form['password']
+    username = request.form.get('username')
+    password = request.form.get('password')
     # Validate data
     if not username:
         return {'msg': 'Username is required!'}, 422
@@ -27,3 +27,22 @@ def register():
     session.add(user)
     session.commit()
     return {'msg': 'User successfully created!'}, 200
+
+
+@bp.route('/login', methods=['POST'])
+def login():
+    # Collect data
+    username = request.form.get('username')
+    password = request.form.get('password')
+    # Validate data
+    if not username:
+        return {'msg': 'Username is required!'}, 422
+    elif not password:
+        return {'msg': 'Password is required!'}, 422
+    # Search user
+    user = session.query(User).filter_by(username=username).first()
+    if not user:
+        return {'msg': 'User with this username don\'t exists'}, 401
+    if not check_password_hash(user.password, password):
+        return {'msg': 'Wrong password!'}, 401
+    return {'msg': 'Authorised!'}, 200
