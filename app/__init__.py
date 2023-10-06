417dc76c388710fd3524b9ba2f2c75b16a70c83c
DerFacn <derfacn.pro228@gmail.com>
2023-10-06 19:05:16 +0200

    Database created

--- app/__init__.py
+++ app/__init__.py
@@ -2,3 +2,20 @@ from flask import Flask
 
 
 app = Flask(__name__)
+db = 'sqlite:///app.db'
+
+
+from app.models import Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+
+engine = create_engine(db)
+Base.metadata.create_all(engine)
+session = scoped_session(sessionmaker(autoflush=False, bind=engine))
+Base.session = session.query_property()
+
+
+@app.teardown_appcontext
+def shutdown_session():
+    session.remove()
---
+++ app/models.py
@@ -0,0 +1,13 @@
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import Mapped, mapped_column
+
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(index=True, unique=True)
+    password: Mapped[str]
